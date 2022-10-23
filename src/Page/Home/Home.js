bb1ba155ee80419149de6476bd0af3ace212c74e
Jonied mirza shakib <MDSHAKIB01088@gmail.com>
2022-10-23 15:59:08 +0600

    complete the blog page succesfull

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BestSellers from '../BestSellers/BestSellers';
+import Blog from '../Blog/Blog';
 import Brand from '../Brand/Brand';
 import NewArrivals from '../NewArrivals/NewArrivals';
 import TopCategories from '../TopCategories/TopCategories';
@@ -16,6 +17,7 @@ const Home = () => {
           <NewArrivals></NewArrivals>
           <TopCategories></TopCategories>
           <Brand></Brand>
+          <Blog></Blog>
         </div>
     );
 };
