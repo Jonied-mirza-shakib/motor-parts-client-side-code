38eea8a026c215498b6688382ffbcafb7ef9d637
Jonied mirza shakib <MDSHAKIB01088@gmail.com>
2022-10-25 20:40:51 +0600

    complete the cart page

--- src/Page/Cart/Cart.js
+++ src/Page/Cart/Cart.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = ({cart}) => {
-    let total=0;
-    for(const product of cart){
-        total=total+product.price;
+const Cart = ({ cart }) => {
+    let total = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product?.quantity;
+        total = total + product.price * product?.quantity;
     }
-    let shipping= parseInt(total*5/100);
-    let grandTotal= total+shipping;
+    let shipping = parseInt(total * 5 / 100);
+    let grandTotal = total + shipping;
     return (
-        <div>
-             <h2>ORDER SUMMERY</h2>
-             <p>TOTAL ITEM: {cart.length}</p>
-             <p>TOTAL PRICE: {total}</p>
-             <p>TOTAL SHIPPING PRICE: {shipping}</p>
-             <h5>GRAND TOTAL: {grandTotal}</h5>
+        <div className='order-summery'>
+            <h2>ORDER SUMMERY</h2>
+            <p>TOTAL ITEM: {quantity}</p>
+            <p>TOTAL PRICE: ${total}</p>
+            <p>TOTAL SHIPPING PRICE: ${shipping}</p>
+            <h5>GRAND TOTAL: ${grandTotal}</h5>
         </div>
     );
 };
--- src/Page/SingleProduct/SingleProduct.js
+++ src/Page/SingleProduct/SingleProduct.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { addToDb, getStoredCart, removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import './SingleProduct.css'
 
@@ -9,15 +10,55 @@ const SingleProduct = () => {
     const [part, setParts] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
-        fetch(`http://localhost:5000/bestSeller/${id}`)
+        fetch('http://localhost:5000/bestSeller')
             .then(res => res.json())
             .then(data => setParts(data))
-    }, [id])
+    }, [])
+    const singleParts = part?.find(parts => parts._id === id);
 
-    const handleAddToCart=product=>{
-        console.log(product)
-        const newCart=[...cart,product];
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const saveCart = [];
+        for (const id in storedCart) {
+            const addedProduct = part.find(products => products._id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct)
+            }
+        }
+        setCart(saveCart)
+    }, [part])
+
+
+
+    const handleIncreaseBtn = selectedProduct => {
+        let newCart = [];
+        const exits = cart.find(allProduct => allProduct._id === selectedProduct._id)
+        if (!exits) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        } else {
+            const rest = cart.filter(allProduct => allProduct._id !== selectedProduct._id)
+            exits.quantity = exits.quantity + 1;
+            newCart = [...rest, exits]
+        }
         setCart(newCart)
+        addToDb(selectedProduct._id)
+    }
+    const handleDecreaseBtn = selectedProduct => {
+        let newCart = [];
+        const exits = cart.find(allProduct => allProduct._id === selectedProduct._id)
+        if (!exits) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        } else {
+            const rest = cart.filter(allProduct => allProduct._id !== selectedProduct._id)
+            exits.quantity = exits.quantity - 1;
+            newCart = [...rest, exits]
+        }
+        setCart(newCart)
+        removeFromDb(selectedProduct._id)
     }
 
     return (
@@ -25,21 +66,21 @@ const SingleProduct = () => {
             <div className='cart-main grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-20 mt-20'>
                 <div>
                     <div className="card w-mx-w bg-base-100 shadow-xl">
-                        <figure><img src={part.img} alt="" className='w-100' /></figure>
+                        <figure><img src={singleParts?.img} alt="" className='w-100' /></figure>
                         <div className="card-body text-center">
-                            <h1 className='best-seller-title'>NAME: {part.name}</h1>
-                            <p className='best-seller-price'>PRICE: ${part.price}</p>
+                            <h1 className='best-seller-title'>NAME: {singleParts?.name}</h1>
+                            <p className='best-seller-price'>PRICE: ${singleParts?.price}</p>
                             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10'>
-                                <button type='button' className='btn btn-outline btn-primary text-xl'>-</button>
-                                <button onClick={()=>handleAddToCart(part)} type='button' className='btn btn-outline btn-primary text-xl'>+</button>
+                                <button onClick={() => handleDecreaseBtn(singleParts)} type='button' className='btn btn-outline btn-primary text-xl'>-</button>
+                                <button onClick={() => handleIncreaseBtn(singleParts)} type='button' className='btn btn-outline btn-primary text-xl'>+</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className="card w-mx-w bg-base-100 shadow-xl">
+                    <div className="card w-mx-w bg-base-200 shadow-xl">
                         <div className='card-body'>
-                           <Cart cart={cart}></Cart>
+                            <Cart cart={cart}></Cart>
                         </div>
                     </div>
                 </div>
